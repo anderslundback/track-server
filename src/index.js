88d4dfe1cfb919ffdae35344c8f49db73d5f269c
Anders Lundback <lundback.anders@gmail.com>
2021-01-04 21:52:22 +0000

    feat: Connect to mongo instance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:korpral13@cluster0.zbalx.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to monto', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there');
